9ac4c77babc522873a42fc51a43a60250a2e8be7
nmk-k66-uet <98223460+nmk-k66-uet@users.noreply.github.com>
2022-03-20 01:14:50 +0700

    Add files via upload
    File main để test class Account

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include "Account.cpp"
+
+int main() {
+    /*Account newAcc;
+    newAcc.createNewAccount();
+    newAcc.saveDataBase();*/
+    /*readDataBase();
+    for (auto x : username) {
+        cout << x << endl;
+    }
+    for (auto y : password) {
+        cout << y << endl;
+    }*/
+    login();
+    return 0;
+}
