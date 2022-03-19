cd9128e6bb5343bc1bab67a1525f7eeabf9243b7
nmk-k66-uet <98223460+nmk-k66-uet@users.noreply.github.com>
2022-03-20 01:13:33 +0700

    Add files via upload
    Class Account đã test log in, sign up, save database

--- Account.cpp
+++ Account.cpp
@@ -2,8 +2,19 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <io.h>
 using namespace std;
+
+vector<string> username;
+vector<string> password;
+
+bool findAcc(vector<string> check, string temp) {
+    for (auto x : check) {
+        if (x == temp) {
+            return true;
+        }
+    }
+    return false;
+}
 
 class Account {
     string username;
@@ -17,13 +28,12 @@ public:
         this -> username = username;
         this -> password = password;
     }
-    void readDataBase () {}; // đọc tất cả tài khoản và mật khẩu đã lưu
     void saveDataBase () { // ghi thêm tài khoản và mật khẩu mới tạo
-        ofstream Username ("D:\Tài liệu Đại học\Lập trình nâng cao\BigAssProject\Account\Username.txt");
-        Username << this -> username;
+        ofstream Username ("D:/Project/Account/Username.txt", ios::app);
+        Username << this -> username << "\n";
         Username.close();
-        ofstream Password ("D:\Tài liệu Đại học\Lập trình nâng cao\BigAssProject\Account\Password.txt");
-        Password << this -> password;
+        ofstream Password ("D:/Project/Account/Password.txt", ios::app);
+        Password << this -> password << "\n";
         Password.close();
     }
     void createNewAccount () {
@@ -59,8 +69,45 @@ public:
         }
         return true;
     }
-    void login() {}; // đăng nhập
     void setPassword (string password) {
         this -> password = password;
     }
 };
+
+
+void readDataBase () { // đọc tất cả tài khoản và mật khẩu đã lưu
+    ifstream Username;
+    Username.open("D:/Project/Account/Username.txt");
+    ifstream Password;
+    Password.open("D:/Project/Account/Password.txt");
+    string temp1;
+    while (!Username.eof()) {
+        Username >> temp1;
+        if (temp1 != "\n") username.push_back(temp1);
+    }
+    Username.close();
+    string temp2;
+    while (!Password.eof()) {
+        Password >> temp2;
+        if (temp2 != "\n") password.push_back(temp2);
+    }
+    Password.close();
+}
+
+void login() {
+    readDataBase();
+    string user, pass;
+    bool check = false;
+    do {
+        cout << "Username: ";
+        cin >> user;
+        cout << "Password: ";
+        cin >> pass;
+        if (findAcc(username, user) && findAcc(password, pass)) {
+            check = true;
+            cout << "Welcome back " << user << "!" << endl;
+        } else {
+            cout << "Invalid username or password!" << endl << "Please renter!" << endl;
+        }
+    } while (check == false);
+}
